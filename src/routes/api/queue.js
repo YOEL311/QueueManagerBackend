ea56d163181d655ab199d09cdb586990ee27e83c
Yoel31 <yoel301@gmail.com>
2021-04-14 00:15:58 +0300

    aa

--- src/routes/api/queue.js
+++ src/routes/api/queue.js
@@ -14,13 +14,13 @@ router.get("/add", async (req, res, next) => {
     body: { queue },
   } = req;
   console.log("🚀 ~ file: queue.js ~ line 16 ~ router.get ~ queue", queue);
+  await pool.query(
+    "CREATE TABLE queue (id SERIAL,full_name varchar,status integer ,timestamp timestamp default current_timestamp);"
+  );
   await pool.query("INSERT INTO queue (full_name,status) values ($1,$2);", [
     queue.fullName,
     "0",
   ]);
-  pool.query(
-    "CREATE TABLE queue (id SERIAL,full_name varchar,status integer ,timestamp timestamp default current_timestamp);"
-  );
 
   pool.query("SELECT * FROM queue WHERE  status = 0;", (errQ, resQ) => {
     res.json(resQ.rows);
