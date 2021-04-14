dc0f70ef0dbbd779456f78c2c31d72f5e34c7875
Yoel31 <yoel301@gmail.com>
2021-04-14 09:23:18 +0300

    add get list route

--- src/routes/api/queue.js
+++ src/routes/api/queue.js
@@ -13,13 +13,13 @@ router.get("/add", async (req, res, next) => {
   const {
     body: { queue },
   } = req;
-  
+
   await pool.query("INSERT INTO queue (full_name,status) values ($1,$2);", [
     queue.fullName,
     "0",
   ]);
 
-  pool.query("SELECT * FROM queue WHERE  status = 0;", (errQ, resQ) => {
+  pool.query("SELECT * FROM queue WHERE  status IN (0, 1);", (errQ, resQ) => {
     res.json(resQ.rows);
   });
 });
@@ -33,8 +33,14 @@ router.get("/nextQueue", async (req, res, next) => {
     UPDATE queue SET  status = 1 WHERE  ID =${queue.currentQueue + 1};`
   );
 
-  pool.query(`SELECT * FROM queue WHERE  status = 1;`, (errQ, resQ) => {
-    res.json(resQ.rows[0].id);
+  pool.query(`SELECT * FROM queue WHERE status IN (0, 1)`, (errQ, resQ) => {
+    resQ && res.json(resQ.rows);
+  });
+});
+
+router.get("/getList", async (req, res, next) => {
+  pool.query(`SELECT * FROM queue WHERE status IN (0, 1)`, (errQ, resQ) => {
+    resQ && res.json(resQ.rows);
   });
 });
 
