b127a7d236fb69500da0eeae2bf07951f20e795d
Yoel31 <yoel301@gmail.com>
2021-04-15 09:17:56 +0300

    add cros

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(
   })
 );
 
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 var indexRouter = require("./src/routes/index");
 require("./src/config/passport");
 app.use(require("./src/routes"));
